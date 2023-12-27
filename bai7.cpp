6df874121427583c0cd8785f1acc3de3edc7f8af
Huy Tuấn <138108656+huytuan033@users.noreply.github.com>
2023-12-27 23:10:31 +0700

    Add files via upload

---
+++ bai7.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+//tạo lớp trừu tượng TinhToan
+class TinhToan
+{
+    protected:
+        int n;
+    
+    public:
+        //hàm khởi tạo
+        TinhToan (int n= 0): n(n)
+        {
+
+        }
+
+        //tạo phương thức trừu tượng tinhtoan
+        virtual double tinhtoan()= 0;
+
+        //hàm lấy giá trị của n
+        int giatriN()
+        {
+            return n;
+        }
+};
+
+//tạo lớp Sum kế thừa từ lớp TinhToan
+class Sum: public TinhToan
+{
+    public:
+        //hàm khởi tạo
+        Sum(int n= 0): TinhToan(n)
+        {
+
+        }
+
+        //nạp chồng toán tử cộng
+        Sum operator+(const Sum& other)
+        {
+            double newSum= this-> n+ other.n;
+            return Sum(newSum);
+        }
+
+        //ghi đè phương thức tinhtoan
+        double tinhtoan() override
+        {
+            double sum= 0.0f;
+            for(int i= 0; i<= n; i++)
+            {
+                sum+= (float)i/(i+1);
+            }
+            return sum;
+        }
+};
+
+int main()
+{
+    int n;
+    cin>> n;
+
+    //tạo đối tượng trong thuộc lớp Sum
+    Sum sum(n);
+    cout<< sum.tinhtoan()<<endl;
+    return 0;
+}
