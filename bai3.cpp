3d82e0c3e6d70d2756f741c7696376e7fe087922
Huy Tuấn <138108656+huytuan033@users.noreply.github.com>
2023-12-27 15:44:59 +0700

    Update bai3.cpp

--- bai3.cpp
+++ bai3.cpp
@@ -45,7 +45,7 @@ class Sum: public Tinh
         double tinh()override
         {
             double sum= 0.0;
-            for(int i= 1; i<= n; i++)
+            for(int i= 0; i<= n; i++)
             {
                 sum+= 1.0/i;
             }
