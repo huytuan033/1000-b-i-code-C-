36cba8d257fdd2b384936164e2b221694d713fea
Huy Tuấn <138108656+huytuan033@users.noreply.github.com>
2023-12-29 11:16:16 +0700

    Add files via upload

---
+++ baii21.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Number
+{
+    protected:
+        int n;
+    
+    public:
+        Number (int n= 0): n(n) {}
+
+        virtual void number() = 0;
+
+        int giatriN()
+        {
+            return n;
+        }
+};
+
+class KetQua: public Number
+{
+    public:
+        KetQua(int n= 0): Number(n) {}
+
+        KetQua operator/ (const KetQua& other)
+        {
+            if (other.n == 0) {
+                throw invalid_argument("Division by zero is not allowed.");
+            }
+            int ketQua= this -> n / other.n;
+            return KetQua(ketQua);
+        }
+
+        void number() override
+        {
+            int sum= 0;
+            for(int i= 1; i<= n; i++)
+            {
+                if(n%i==0)
+                {
+                    sum+= i;
+                }
+            }
+            cout << sum;
+        }
+};
+
+int main()
+{
+    int n;
+    cin>> n;
+    KetQua ketqua(n);
+    cout << "Tong cac uoc so cua " << n << " la: ";
+    ketqua.number();
+    cout << endl;
+    return 0;
+}
